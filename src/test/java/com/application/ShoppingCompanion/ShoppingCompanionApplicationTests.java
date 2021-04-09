0d80a2a5bef473b850bf6750eb066e9827293309
Warren A Stowe <stowewa@gmail.com>
2021-04-09 19:07:13 -0400

    initial commit

---
+++ src/test/java/com/application/ShoppingCompanion/ShoppingCompanionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.ShoppingCompanion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCompanionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
